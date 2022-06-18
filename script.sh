f96140a5a3831d9e3c99fe14fc95ef2fdb50ae5d
david <david.m.niu@gmail.com>
2022-06-18 18:06:13 -0400

    added .bashrc

--- script.sh
+++ script.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-myRepo = "https://github.com/davidmniu/templates"
+myRepo="https://raw.githubusercontent.com/davidmniu/wslConfig/main/"
 
 # Vim Zenburn
 mkdir -p /home/david/.vim/colors
 cd /home/david/.vim/colors
+rm zenburn.vim
 wget https://raw.githubusercontent.com/jnurmine/Zenburn/master/colors/zenburn.vim
 
 # vimrc, bashrc
 cd /home/david/
-wget $myRepo/.vimrc
-wget $myRepo/.bashrc
+rm .vimrc
+rm .bashrc
+wget $myRepo.vimrc
+wget $myRepo.bashrc
 
 # Install Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -20,7 +23,7 @@ vim +PluginInstall +qall
 sudo apt install build-essential cmake vim-nox python3-dev
 sudo apt install mono-complete golang nodejs default-jdk npm
 cd ~/.vim/bundle/YouCompleteMe
-python3 install.py --all
+python3 install.py --clangd-completer
 
 # Set up cpp dev
 wget $myRepo/.ycm_extra_conf.py
@@ -29,5 +32,5 @@ cd /home/david/templates/
 wget $myRepo/complate.cpp
 
 # Script for Windows Terminal
-cd /mnt/
+cd /mnt/c/Users/david/OneDrive/Desktop
 wget $myRepo/terminal.ahk
